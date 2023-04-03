d147d46e74c25d0fe7a0932b3e8a95b32fdd1c5e
Jack Kamaryt <jkamaryt46@gmail.com>
2023-04-03 10:02:36 -0500

    Initial Commit

---
+++ Minecraft Recipe HQ/Minecraft_Recipe_HQApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minecraft_Recipe_HQApp.swift
+//  Minecraft Recipe HQ
+//
+//  Created by Jack Kamaryt on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Minecraft_Recipe_HQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
